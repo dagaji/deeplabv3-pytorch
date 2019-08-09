6e8d5f4134b5d88bf550efd355bb1cc9b9e14d63
David <davidgj1994@gmail.com>
2019-08-09 17:32:46 +0200

    separate h and v lines during training

---
+++ deeplabv3/dataset/lines.py
@@ -0,0 +1,143 @@
+import matplotlib
+matplotlib.use('tkagg')
+import random
+import numpy as np
+import torch
+from torch.utils import data
+import os.path
+import os
+import pdb
+from torchvision import transforms
+from skimage.io import imread
+import torchvision.transforms.functional as TF
+import cv2
+import matplotlib.pyplot as plt
+from scipy import ndimage
+import deeplabv3.lines as lines
+import pickle
+from scipy.spatial import distance
+
+class LinesDatasetFactory:
+
+    bbox_size = (833, 449)
+    M = 50
+
+    def norm_coords(self, coords):
+        x_norm = float(self.bbox_size[0] - 1) / 2
+        y_norm = float(self.bbox_size[1] - 1) / 2
+        coords[:, 0] = coords[:,0] / x_norm - 1
+        coords[:, 1] = coords[:, 1] / y_norm - 1
+        return coords
+
+    def line_coords(self, intersect_points, orientation, n_points, plot=False):
+
+        if plot:
+            fig, ax = plt.subplots(1)
+            ax.imshow(np.zeros(self.bbox_size[::-1] + (3,), dtype=np.uint8))
+
+        step_len = distance.euclidean(intersect_points[0], intersect_points[1]) / n_points
+        orientation_rad = np.deg2rad(orientation)
+        unit_vector = np.array((np.sin(orientation_rad), np.cos(orientation_rad)))
+        line_points = []
+        for i in range(1, n_points):
+            line_point = np.array(intersect_points[0]) + i * step_len * unit_vector
+            line_points.append(line_point)
+            if plot:
+                circle = plt.Circle(tuple(line_point.tolist()), 2, color='b')
+                ax.add_patch(circle)
+
+        if plot:
+            plt.show()
+        line_points = np.array(line_points)
+        return self.norm_coords(line_points)
+
+
+    def compute_anchors_grid(self):
+
+        anchors_v_intersect = lines.get_anchors(0.0, 50, self.bbox_size[::-1], is_vertical=True)
+        anchors_h_intersect = lines.get_anchors(90.0, 25, self.bbox_size[::-1], is_vertical=False)
+
+        grids_v = []
+        for intersect in anchors_v_intersect:
+            grid = self.line_coords(intersect, 0.0, self.M)
+            grid = grid.reshape(grid.shape[0],1,2)
+            grids_v.append(grid)
+
+        grids_h = []
+        for intersect in anchors_h_intersect:
+            grid = self.line_coords(intersect, 90.0, 2 * self.M)
+            grid = grid.reshape(grid.shape[0],1,2)
+            grids_h.append(grid)
+
+        return grids_v, grids_h
+
+
+    def __init__(self, root):
+
+        with open(os.path.join(root, 'lines_gt.pickle'), "rb") as file:
+            self.lines_gt = pickle.load(file)
+
+        self.grids_v, self.grids_h = self.compute_anchors_grid()
+
+    def get_line_dataset(self, image_id):
+        lines_gt_v, lines_gt_h = self.lines_gt[image_id]['v'], self.lines_gt[image_id]['h']
+        lines_gt_total = np.concatenate((lines_gt_v, lines_gt_h))
+        grid_total = self.grids_v + self.grids_h
+        return LinesDataset(grid_total, lines_gt_total)
+
+
+class LinesDataset(data.Dataset):
+
+    def __init__(self, grids, gts):
+        self.grids = grids
+        self.gts = gts
+
+    def __getitem__(self, index):
+        grid = self.grids[index]
+        gt = self.gts[index]
+        return grid, gt
+
+    def __len__(self):
+        return len(self.grids)
+
+
+
+class RotatedImagesDataset(data.Dataset):
+
+    
+    def __init__(self, root, id_list_path):
+
+        
+        self.root = root
+        id_list = np.loadtxt(id_list_path, dtype=str)
+        self.id_list = []
+        for _id in id_list:
+            if "APR" in _id:
+                self.id_list.append(_id)
+        self.mean = [0.485, 0.456, 0.406]
+        self.var = [0.229, 0.224, 0.225]
+
+    def _load_data(self, idx):
+        """
+        Load the image and label in numpy.ndarray
+        """
+        image_id = self.id_list[idx] + '.png'
+        img_path = os.path.join(self.root, "images", image_id)
+        img = np.asarray(imread(img_path))
+        return image_id, img
+
+    def __getitem__(self, index):
+
+        image_id, image = self._load_data(index)
+        image = TF.to_tensor(image)
+        image = TF.normalize(image, self.mean, self.var)
+        image = image.numpy()
+        return dict(image_id=image_id, image=image)
+
+    def __len__(self):
+        return len(self.id_list)
+
+    def __repr__(self):
+        fmt_str = "     Dataset: " + self.__class__.__name__ + "\n"
+        fmt_str += "    Root: {}".format(self.root)
+        return fmt_str
--- train_lines.py
+++ train_lines.py
@@ -1,13 +1,21 @@
 import torch
 from deeplabv3.metrics import AverageMeter
 from tqdm import tqdm
-from argparse import ArgumentParser
 from pathlib import Path
-import pdb
 import torch.nn as nn
 from train import set_bn_eval
-import deeplabv3.dataset as dataset
-import deeplabv3.model as model
+import deeplabv3.dataset.lines as lines_datasets
+import deeplabv3.model.lines as lines_models
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import pdb
+import time
+from tqdm import tqdm
+from skimage.io import imsave, imread
+import os.path
+from argparse import ArgumentParser
 
 def parse_args():
 	parser = ArgumentParser()
@@ -17,28 +25,65 @@ def parse_args():
 
 root = '/home/davidgj/projects/APR_rotated'
 checkpoint_dir = '/home/davidgj/projects/deeplabv3-pytorch/checkpoint'
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 args = parse_args()
+
 checkpoint_path = os.path.join(checkpoint_dir, 
 							   args.dataset, 
 							   'partition_{}', 
 							   'decoder-s8-res1000-multitask', 
 							   'epoch_8.pth').format(args.partition)
-
+seg_model = lines_models.get_seg_model().to(device)
 checkpoint_dict = torch.load(checkpoint_path)
-model_train.load_state_dict(last_checkpoint["model_state_dict"])
-			current_epoch = last_checkpoint["epoch"]
-			optimizer.load_state_dict(last_checkpoint["optimizer_state_dict"])
+seg_model.load_state_dict(checkpoint_dict["model_state_dict"], strict=False)
+for param in seg_model.parameters():
+	param.requires_grad = False
+seg_model.eval()
+detection_model = lines_models.LineDetectionNet().to(device)
 
 list_dir = os.path.join('list', args.dataset, 'partition_{}').format(args.partition)
 train_id_txt = os.path.join(list_dir, 'train.txt')
-val_id.txt = os.path.join(list_dir, 'val.txt')
+val_id_txt = os.path.join(list_dir, 'val.txt')
 
-img_dataset_train = dataset.RotatedImagesDataset(root, train_id_txt)
-img_dataset_val = dataset.RotatedImagesDataset(root, val_id_txt)
+img_dataset_train = lines_datasets.RotatedImagesDataset(root, train_id_txt)
+img_dataset_val = lines_datasets.RotatedImagesDataset(root, val_id_txt)
 
 img_loader_train = torch.utils.data.DataLoader(img_dataset_train, batch_size=1, shuffle=True, num_workers=0)
 img_loader_val = torch.utils.data.DataLoader(img_dataset_val, batch_size=1, shuffle=False, num_workers=0)
 
-lines_factory = dataset.LinesDatasetFactory(root)
+lines_factory = lines_datasets.LinesDatasetFactory(root)
+
+criterion = nn.BCELoss()
+criterion = criterion.to(device)
+
+params_to_update = detection_model.parameters()
+optimizer = optim.SGD(params_to_update, lr= 0.0005, momentum=0.9, weight_decay=1e-5)
+
+running_loss = AverageMeter()
+running_loss.reset()
+
+for outer_iter, data in tqdm(enumerate(img_loader_train), total=len(img_loader_train), dynamic_ncols=True):
+
+	image_id = data['image_id']
+	image = data['image'].to(device)
+	features = seg_model(image)
+	line_loader = torch.utils.data.DataLoader(lines_factory.get_line_dataset(image_id[0]), batch_size=15, shuffle=True, num_workers=0)
+
+	optimizer.zero_grad()
+	running_loss.reset()
+	pdb.set_trace()
+	for inner_iter, (grids, gts) in enumerate(line_loader):
+		grids = grids.to(device)
+		gts = gst.to(device)
+		lines_scores = detection_model(features, grids)
+		loss = criterion(lines_scores, gts) / len(line_loader)
+		loss.backward()
+		running_loss.update(loss.item() * len(line_loader), n=grids.size(0))
+	optimizer.step()
+	print('>>> Loss: {:.4f}'.format(running_loss.value()))
+
+
+
+
 
